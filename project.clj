72496d57247b9fc2b45b67ab54df81f3e2dfd022
Crispin Wellington <retrogradeorbit@gmail.com>
2020-05-20 01:11:50 +0800

    reflection patch

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.2-alpha1"]
                  [borkdude/sci "0.0.13-alpha.23"]]
+  :java-source-paths ["src/java"]
   :main ^:skip-aot locking-test.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
