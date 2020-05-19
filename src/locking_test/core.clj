b40794b45cad19d56737f0677548205e3f7e296d
Crispin Wellington <retrogradeorbit@gmail.com>
2020-05-20 01:08:03 +0800

    boom

--- src/locking_test/core.clj
+++ src/locking_test/core.clj
@@ -1,4 +1,8 @@
 (ns locking-test.core
+  (:require [clojure.tools.cli :refer [parse-opts]]
+            [clojure.string :as string]
+            [clojure.java.io :as io]
+            [sci.core :as sci])
   (:gen-class))
 
 (def ^:dynamic *unrestricted* false)
@@ -91,4 +95,34 @@
   [& args]
   (let [v (myvar. 0 'foo nil)]
     (alter-var-root* v inc)
-    (println @v)))
+    (println @v))
+
+  (let [ctx {:namespaces {}
+             :bindings {}
+             :imports {'System 'java.lang.System}
+             :classes {'java.lang.System System
+                       'java.time.Clock java.time.Clock
+                       'java.time.DateTimeException java.time.DateTimeException
+                       'java.time.DayOfWeek java.time.DayOfWeek
+                       'java.time.Duration java.time.Duration
+                       'java.time.Instant java.time.Instant
+                       'java.time.LocalDate java.time.LocalDate
+                       'java.time.LocalDateTime java.time.LocalDateTime
+                       'java.time.LocalTime java.time.LocalTime
+                       'java.time.Month java.time.Month
+                       'java.time.MonthDay java.time.MonthDay
+                       'java.time.OffsetDateTime java.time.OffsetDateTime
+                       'java.time.OffsetTime java.time.OffsetTime
+                       'java.time.Period java.time.Period
+                       'java.time.Year java.time.Year
+                       'java.time.YearMonth java.time.YearMonth
+                       'java.time.ZonedDateTime java.time.ZonedDateTime
+                       'java.time.ZoneId java.time.ZoneId
+                       'java.time.ZoneOffset java.time.ZoneOffset
+                       'java.time.temporal.TemporalAccessor java.time.temporal.TemporalAccessor
+                       'java.time.format.DateTimeFormatter java.time.format.DateTimeFormatter
+                       'java.time.format.DateTimeFormatterBuilder java.time.format.DateTimeFormatterBuilder
+                       }}]
+    (println
+     (-> "(inc 1)"
+         (sci/eval-string ctx)))))
