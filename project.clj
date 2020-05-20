f86e7003770528ca9a9cbd0dcc35b7635af11044
Crispin Wellington <retrogradeorbit@gmail.com>
2020-05-20 12:05:54 +0800

    exclude inbuild sci reflector

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.2-alpha1"]
-                 [borkdude/sci "0.0.13-alpha.23"]]
+                 [borkdude/sci "0.0.13-alpha.23" :exclusions
+                  [borkdude/sci.impl.reflector]]]
   :java-source-paths ["src/java" "sci-reflector/src-java"]
   :main ^:skip-aot locking-test.core
   :target-path "target/%s"
