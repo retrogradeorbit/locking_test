e6e0aaf8d564ef4bd1fce06f553bbd52404cc395
Crispin Wellington <retrogradeorbit@gmail.com>
2020-05-20 20:49:00 +0800

    segfaulting binary

--- project.clj
+++ project.clj
@@ -3,10 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.2-alpha1"]
-                 [borkdude/sci "0.0.13-alpha.23" :exclusions
-                  [borkdude/sci.impl.reflector]]]
-  :java-source-paths ["src/java" "sci-reflector/src-java"]
+  :dependencies [[org.clojure/clojure "1.10.2-alpha1"]]
+  :java-source-paths ["src/java"]
   :main ^:skip-aot locking-test.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/locking_test/core.clj
+++ src/locking_test/core.clj
@@ -1,14 +1,44 @@
 (ns locking-test.core
-  (:require [sci.core :as sci])
   (:gen-class))
+
+(defmacro with-writeable-var
+  [the-var var-meta & body]
+  `(let [vm# ~var-meta]
+     (if (not (:sci.impl/built-in vm#))
+       (do ~@body)
+       (let [the-var# ~the-var
+             ns# (:ns vm#)
+             ns-name# (getName ns#)
+             name# (getName the-var#)]
+         (throw (ex-info (str "Built-in var #'" ns-name# "/" name# " is read-only.")
+                         {:var ~the-var}))))))
+
+(defprotocol IVar
+  (bindRoot [this v])
+  (getName [this]
+    (:name (meta this))))
+
+(deftype SciVar [^:volatile-mutable root
+                 ^:volatile-mutable meta]
+  IVar
+  (bindRoot [this v]
+    (set! root v))
+  clojure.lang.IDeref
+  (deref [this] root)
+  clojure.lang.IReference
+  (alterMeta [this f args]
+    (with-writeable-var this meta
+      (locking (set! meta (apply f meta args)))))
+  (resetMeta [this m]
+    (locking (set! meta m))))
+
+(defn alter-sci-var-root [v f & args]
+  (locking v (bindRoot v (apply f @v args))))
 
 (defn -main
   "locking test"
-  [& args]
-  (let [ctx {:namespaces {}
-             :bindings {}
-             :imports {}
-             :classes {}}]
-    (-> "1"
-        (sci/eval-string ctx)
-        println)))
+  [& _args]
+  (let [var (->SciVar 1 {})]
+    (alter-sci-var-root var #(+ 1 %))
+    (alter-meta! var (constantly {:a 1}))
+    (prn @var (meta var))))
